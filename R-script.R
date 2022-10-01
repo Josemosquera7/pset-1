1920329c794fba272ed0413644efdbca521271c9
Josemosquera7 <je.mosqueral1@uniandes.edu.co>
2022-10-01 13:05:33 -0500

    Update R-script.R

--- R-script.R
+++ R-script.R
@@ -1,10 +1,15 @@
 #Santiago Huertas
 #Jose Mosquera 202110281
 #David Chaparro 202023984
+#R version 4.2.1 (2022-06-23 ucrt)
 rm(list=ls())
 require(pacman)
 p_load(rio)
 p_load(tidyverse)
 p_load(dplyr, tidyr, tibble, data.table) 
 #1 Vectores
-vector_1<-1:100
+vector_pares<-1:100
+vector_impares <- seq(1,99,by=2)
+vector_3 <- 1:100 
+vector_f<- vector_3[-(vector_impares)]
+#2
